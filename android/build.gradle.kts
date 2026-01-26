c2128b532f331720a27d1bd466425306e6894c17
Charles R. Portwood II <charlesportwoodii@erianna.com>
2026-01-25 20:21:53 -0600

    chore: update android path

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.alaydriem.bvc.plugin.keyring"
+    namespace = "com.charlesportwoodii.tauri.plugin.keyring"
     compileSdk = 36
 
     defaultConfig {
@@ -37,11 +37,11 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
-    
+
     // Basic testing dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    
+
     implementation(project(":tauri-android"))
 }
